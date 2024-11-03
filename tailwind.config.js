1e2919897cacabc419c21bf0f11f91780572f5bf
qabbes <quentin.abbes@gmail.com>
2024-11-03 16:32:22 +0100

    installed dependencies + global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,33 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+    },
+
+    fontFamily: {
+      primary: "var(--font-bebas-neue)",
+      secondary: "var(--font-inter)",
+    },
+
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#10151f",
+        secondary: "#475467",
+        accent: {
+          DEFAULT: "#7edad2",
+          hover: "#79d3cc",
+        },
+      },
+      boxShadow: {
+        custom: "0px 14px 54px rgba(0, 0, 0, 0.1)",
       },
     },
   },
