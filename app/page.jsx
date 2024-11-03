c00513482f2b650d61b133b4f580f0423181e904
qabbes <quentin.abbes@gmail.com>
2024-11-03 20:36:05 +0100

    Hero component set up

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Hero from "@/components/Hero";
 
 const Home = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
---
+++ components/Hero.jsx
@@ -0,0 +1,53 @@
+"use client";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { Link as ScrollLink } from "react-scroll";
+
+const Hero = () => {
+  return (
+    <section className="h-[800px] relative bg-accent/10 xl:bg-white">
+      {/* Header */}
+      <div>header</div>
+      <div className="container mx-auto h-full">
+        <div
+          className="relative z-20 h-full w-full xl:max-w-[768px] flex flex-col items-center
+        xl:items-start justify-center text-center xl:text-left pt10">
+          <h1 className="h1 mb-2 max-w-[320px] xl:max-w-none">
+            <span className="text-accent">I Build And</span> Design Powerful
+            <TypeAnimation
+              preRenderFirstString={true}
+              sequence={["Websites", 2000, "Apps", 2000]}
+              speed={50}
+              repeat={Infinity}
+              wrapper="span"
+              cursor={false}
+              className="ml-2 xl:ml-4"
+            />
+          </h1>
+          <p className="lead max-w-[476px] mb-6">
+            Delivering powerful, custom websites that blends aesthetics with performance
+          </p>
+          <ScrollLink to="contact" smooth>
+            <button className="btn btn-accent mb-8">Get In Touch</button>
+          </ScrollLink>
+          {/* Stats  */}
+          <div>stats</div>
+        </div>
+        {/* Image */}
+        <div>
+          <div>
+            <Image
+              src="/assets/about/pose1.png"
+              fill
+              quality="100"
+              priority
+              className="object-contain"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
