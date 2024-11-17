dbf87630a0ed84f6115873f36108686e06ea8494
qabbes <quentin.abbes@gmail.com>
2024-11-17 23:53:46 +0100

    added OG data

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter, Bebas_Neue } from "next/font/google";
 
+
 const inter = Inter({
   weights: ["100..900"],
   subsets: ["latin"],
@@ -14,8 +15,26 @@ const bebasNeue = Bebas_Neue({
 });
 
 export const metadata = {
-  title: "Quentin Abbes",
-  description: "Quentin Abbes Portfolio",
+  openGraph: {
+    title: "Quentin Abbes",
+    description: "Quentin Abbes Portfolio",
+
+    images: [
+      {
+        url: "/assets/work/opengraph-img.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "/assets/work/opengraph-img.png",
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
